51956bcae6361e257bafeea50cf3a68eec3f2d1d
RyoDagg <mannai.abdlkader@gmail.com>
2022-01-07 11:55:40 +0100

    Automatically generate 'Robes' list based on model's quantity

--- models/robe_occasion.py
+++ models/robe_occasion.py
@@ -22,28 +22,22 @@ class RobeOccasion(models.Model):
     )
 
     nbr_amort = fields.Integer(
-        string="Nombre d'mortissement",
+        string="Nombre d'mortissement"
         default = 0,
         compute = 'calc_nbr_amort'
     )
 
-    #
-    # def _generate_ref(self):
-    #     for robe in self:
-    #         # code = str(self.env.search_count([('', '=', 'entry')]))
-    #         robe.ref = str(robe.type).upper() + '#' + str(robe.id).upper()
+    # @api.depends('reservation_ids')
+    # def calc_nbr_amort(self):
+    #     for record in self:
+    #         if record.reservation_ids:
+    #             record.nbr_amort = len(record.reservation_ids)
+    #         else:
+    #             record.nbr_amort = 0
 
-    @api.depends('reservation_ids')
-    def calc_nbr_amort(self):
-        for record in self:
-            if record.reservation_ids:
-                record.nbr_amort = len(record.reservation_ids)
-            else:
-                record.nbr_amort = 0
-
-    def name_get(self):
-        result = []
-        for record in self:
-            rec_name = "%s" %(record.nom)
-            result.append((record.id, rec_name))
-        return result
+    # def name_get(self):
+    #     result = []
+    #     for record in self:
+    #         rec_name = "%s" %(record.nom)
+    #         result.append((record.id, rec_name))
+    #     return result
